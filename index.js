9cb42bbbe2670e142f77824ab6b48a6499d4d84d
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-07 17:08:31 -0400

    DAY TWO MVP COMPLETE

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const db = require("./data/dbHelp");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 const server = express();
 
 server.use(express.json());
+
+const sessionConfig = {
+  name: "cookie",
+  secret: "secret",
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 1,
+    secure: false
+  },
+  resave: false,
+  saveUninitialized: true
+};
+
+server.use(session(sessionConfig));
 
 server.get("/", (req, res) => {
   res.send("server running..");
@@ -36,10 +51,11 @@ server.post("/api/register", (req, res) => {
 
 server.post("/api/login", (req, res) => {
   const { username, password } = req.body;
-  db.login({ username: username })
+  db.login({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ message: `Welcome ${user.username}` });
       } else {
         res.status(404).json({ message: "Invaild Login" });
@@ -48,6 +64,20 @@ server.post("/api/login", (req, res) => {
     .catch(err => {
       res.status(500).json(err);
     });
+});
+
+server.get("/api/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("error logging out");
+      } else {
+        res.send("Logged Out");
+      }
+    });
+  } else {
+    res.end();
+  }
 });
 
 server.listen(5000, () => {
